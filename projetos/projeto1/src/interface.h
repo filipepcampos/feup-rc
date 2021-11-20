4fc8a1691485a15707a6d65b042975c9cff964dc
Vasco52 <64026324+Vasco52@users.noreply.github.com>
2021-11-20 13:36:41 +0000

    Add interface header

---
+++ projetos/projeto1/src/interface.h
@@ -0,0 +1,21 @@
+#define MAX_SIZE 1025
+
+typedef struct {
+    int fileDescriptor; // Descritor correspondente à porta série
+    int status; // TRANSMITTER | RECEIVER
+} applicationLayer;
+
+/*typedef struct {
+    char port[20]; // Dispositivo /dev/ttySx, x = 0, 1
+    int baudRate; // Velocidade de transmissão
+    unsigned int sequenceNumber; // Número de sequência da trama: 0, 1
+    unsigned int timeout; // Valor do temporizador: 1 s
+    unsigned int numTransmissions; // Número de tentativas em caso de falha
+    char frame[MAX_SIZE]; // Trama
+} linkLayer; */
+
+
+// int llopen(int port, ...);
+int llwrite(int fd, char * buffer, int length);
+int llread(int fd, char * buffer);
+int llclose(int fd);
