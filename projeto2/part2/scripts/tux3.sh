4a522d194e8baba050590a7f798b001557760941
Filipe Campos <filipepcampos24@gmail.com>
2022-01-25 21:10:39 +0000

    Generalize our scripts

--- projeto2/part2/scripts/tux3.sh
+++ projeto2/part2/scripts/tux3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ip addr flush eth0
-ip addr add 172.16.20.1/24 dev eth0
-ip route add 172.16.21.0/24 via 172.16.20.254
-ip route add default via 172.16.20.254
+ip addr add 172.16.Y0.1/24 dev eth0
+ip route add 172.16.Y1.0/24 via 172.16.Y0.254
+ip route add default via 172.16.Y0.254
--- projeto2/part2/scripts/tux4.sh
+++ projeto2/part2/scripts/tux4.sh
@@ -2,8 +2,8 @@
 
 ip addr flush eth0
 ip addr flush eth1
-ip addr add 172.16.20.254/24 dev eth0
-ip addr add 172.16.21.253/24 dev eth1
-ip route add default via 172.16.21.254
+ip addr add 172.16.Y0.254/24 dev eth0
+ip addr add 172.16.Y1.253/24 dev eth1
+ip route add default via 172.16.Y1.254
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
